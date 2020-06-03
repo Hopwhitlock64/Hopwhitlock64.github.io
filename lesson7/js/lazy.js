1645e1c1ab24cf8dd2dd7cd5eedc2de9c7039cf8
hopwhitlock64 <whi15064@byui.edu>
2020-06-03 10:54:11 -0600

    lesson7

---
+++ lesson7/js/lazy.js
@@ -0,0 +1,25 @@
+const images = document.querySelectorAll('[data-src]');
+
+function preloadImage(img){
+    const source = img.getAttribute('data-src');
+    if(!source){
+        return;
+    }
+    img.src = source;
+}
+const options = {threshold: [.5]};
+const io = new IntersectionObserver(
+    (entries, io) => {
+        entries.forEach(entry => {
+            if(!entry.isIntersecting){
+                return;
+            } else{
+                preloadImage(entry.target); //entry.target becomes img
+                io.unobserve(entry.target); // once shown, stops observing
+            }
+        }); 
+    }, options
+); 
+images.forEach(image => {
+    io.observe(image);
+});
