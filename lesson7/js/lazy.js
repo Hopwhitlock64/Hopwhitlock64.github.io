1ce62fd389bfd56307267a83ba72d3e4b659d449
hopwhitlock64 <whi15064@byui.edu>
2020-06-04 17:05:19 -0600

    gallery grid

--- lesson7/js/lazy.js
+++ lesson7/js/lazy.js
@@ -1,3 +1,4 @@
+// lazy loader
 const images = document.querySelectorAll('[data-src]');
 
 function preloadImage(img){
@@ -28,3 +29,19 @@ io.unobserve(entry.target);
 images.forEach(image => {
 io.observe(image);
 });
+
+//Hamburger menu
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('.ham');
+
+hambutton.addEventListener('click', 
+() => {mainnav.classList.toggle('responsive')}, false);
+
+// Footer Date
+let week_name = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let month_name = ['January','February', 'March', 'April', 'May','June', 'July', 'August', 'September', 'October', 'November', 'December'];
+let mydate = new Date();
+let weekname = week_name[mydate.getDay()];
+let monthname  = month_name[mydate.getMonth()];
+const dateoutput = document.querySelector('.currentdate');
+dateoutput.textContent = weekname + ', ' + mydate.getDate() + " " + monthname + " " + mydate.getFullYear();
