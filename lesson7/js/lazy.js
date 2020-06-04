cb9ab77b269eb27e00c241cf53b1f54dcaaa57a6
hopwhitlock64 <whi15064@byui.edu>
2020-06-04 13:45:06 -0600

    lazy loading fixed?

--- lesson7/js/lazy.js
+++ lesson7/js/lazy.js
@@ -1,25 +1,30 @@
 const images = document.querySelectorAll('[data-src]');
 
 function preloadImage(img){
-    const source = img.getAttribute('data-src');
-    if(!source){
-        return;
-    }
-    img.src = source;
+const source = img.getAttribute('data-src');
+if(!source) {
+return;
 }
-const options = {threshold: .5};
-const io = new IntersectionObserver(
-    (entries, io) => {
-        entries.forEach(entry => {
-            if(!entry.isIntersecting){
-                return;
-            } else{
-                preloadImage(entry.target); //entry.target becomes img
-                io.unobserve(entry.target); // once shown, stops observing
-            }
-        }); 
-    }, options
-); 
+
+img.src = source;
+}
+
+const options = {threshold: [.5]};
+
+const io = new IntersectionObserver (
+(entries, io) => {
+entries.forEach(entry => {
+if (!entry.isIntersecting) {
+return;
+} else {
+preloadImage(entry.target);
+io.unobserve(entry.target);
+}
+});
+}, options
+);
+
+
 images.forEach(image => {
-    io.observe(image);
+io.observe(image);
 });
