a26d31cec270658e1e35bee673dcd4480c283e7f
hopwhitlock64 <whi15064@byui.edu>
2020-06-18 14:45:58 -0600

    lesson 9 commit 2

--- lesson9/js/home.js
+++ lesson9/js/home.js
@@ -9,49 +9,52 @@ fetch(recieveUrl)
     const towns = jsonObject['towns'];
 
     for (let i = 0; i < towns.length; i++ ) {
-        let sections = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let h3 = document.createElement('h3');
-        let par1 = document.createElement('p');
-        let par2 = document.createElement('p');
-        let par3 = document.createElement('p');
-        let image = document.createElement('img');
+      if(towns[i].name == "Preston" || towns[i].name == "Soda Springs" || towns[i].name == "Fish Haven"){
+          let card = document.createElement('section');
+          let h2 = document.createElement('h2');
+          let h3 = document.createElement('h3');
+          let par1 = document.createElement('p');
+          let par2 = document.createElement('p');
+          let par3 = document.createElement('p');
+          let image = document.createElement('img');
+  
+          
+          h2.textContent = towns[i].name;
+          card.appendChild(h2);
+          card.classList.add('section');
+          document.querySelector('div.cards').appendChild(card);
+  
+          h3.textContent = towns[i].motto;
+          card.appendChild(h3);
+          card.classList.add('section');
+          document.querySelector('div.cards').appendChild(card);
+  
+          par1.textContent = 'Year Founded: ' + towns[i].yearFounded;
+          card.appendChild(par1);
+          card.classList.add('section');
+          document.querySelector('div.cards').appendChild(card);
+  
+          par2.textContent = 'Population: ' + towns[i].currentPopulation;
+          card.appendChild(par2);
+          card.classList.add('section');
+          document.querySelector('div.cards').appendChild(card);
+  
+          par3.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+          card.appendChild(par3);
+          card.classList.add('section');
+          document.querySelector('div.cards').appendChild(card);
+  
+          image.setAttribute("src", "https://Hopwhitlock64.github.io/lesson9/images/" + towns[i].photo);
+          image.setAttribute("alt", "picture of the town");
+          card.appendChild(image);
+          card.classList.add('section');
+  
+          document.querySelector('div.cards').appendChild(card);
+        
 
-        h2.textContent = towns[i].name;
-        card.appendChild(h2);
-        card.classList.add('sections');
-        document.querySelector('div.cards').appendChild(card);
-
-        h3.textContent = towns[i].motto;
-        card.appendChild(h3);
-        card.classList.add('sections');
-        document.querySelector('div.cards').appendChild(card);
-
-        par1.textContent = 'Year Founded: ' + towns[i].yearFounded;
-        card.appendChild(par1);
-        card.classList.add('sections');
-        document.querySelector('div.cards').appendChild(card);
-
-        par2.textContent = 'Population: ' + towns[i].currentPopulation;
-        card.appendChild(par2);
-        card.classList.add('sections');
-        document.querySelector('div.cards').appendChild(card);
-
-        par3.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
-        card.appendChild(par3);
-        card.classList.add('sections');
-        document.querySelector('div.cards').appendChild(card);
-
-        image.setAttribute("src", towns[i].photo);
-        image.setAttribute("alt", "picture of the town");
-        card.appendChild(image);
-        card.classList.add('sections');
-
-        document.querySelector('div.cards').appendChild(card);
-
-        let onlyTowns = recieveUrl;
-        let town_filter = onlyTowns.filter( jsonObject => jsonObject.towns[i].name =="Preston")
-console.log(town_filter)
+        
+      }
+     
     }
   })
   .catch(function(error){
